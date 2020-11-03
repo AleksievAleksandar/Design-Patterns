eb1dbc169535aaabec633eb9c74a36f6f09214a5
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-11-03 19:31:00 +0200

    Add files via upload

---
+++ 03.Decorator Pattern/Beverage.h
@@ -0,0 +1,65 @@
+#ifndef BEVERAGE_H
+#define BEVERAGE_H
+
+class Beverage
+{
+public:
+	virtual double cost() = 0;
+	virtual ~Beverage() = default;
+};
+
+class AddDecorator : public Beverage
+{
+public:
+	virtual double cost() = 0;
+	virtual ~AddDecorator() = default;
+};
+
+class Espresso : public Beverage
+{
+public:
+	double cost() override
+	{
+		return 1.5;
+	}
+};
+
+class Caramel : public AddDecorator
+{
+protected:
+	Beverage* beverage;
+
+public:
+	Caramel(Beverage* beverage) :
+		beverage(beverage) {}
+	~Caramel()
+	{
+		delete this->beverage;
+	}
+public:
+	double cost()
+	{
+		return this->beverage->cost() + 2.3;
+	}
+};
+
+class Milk : public AddDecorator
+{
+protected:
+	Beverage* beverage;
+
+public:
+	Milk(Beverage* beverage) :
+		beverage(beverage) {}
+	~Milk()
+	{
+		delete this->beverage;
+	}
+public:
+	double cost()
+	{
+		return this->beverage->cost() + 1.4;
+	}
+};
+
+#endif // !BEVERAGE_H

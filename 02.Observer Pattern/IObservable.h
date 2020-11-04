9efde44726d9df7cf3e223638353742d38eb0f42
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-11-04 17:02:50 +0200

    Update IObservable.h

--- 02.Observer Pattern/IObservable.h
+++ 02.Observer Pattern/IObservable.h
@@ -5,6 +5,7 @@ class IObserver
 {
 public:
 	virtual void update() = 0;
+	virtual ~IObserver() = default;
 };
 
 class IObservable
@@ -13,6 +14,7 @@ public:
 	virtual void add(IObserver* observer) = 0;
 	virtual void remove(IObserver* observer) = 0;
 	virtual void notify() = 0;
+	virtual ~IObservable() = default;
 };
 
 #endif // !I_OBSERVABLE_H

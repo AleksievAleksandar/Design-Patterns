58819957c70c886f370305063b8470c898bec05f
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-11-17 21:28:40 +0200

    Create Main.cpp

---
+++ 10.Proxy Pattern/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+#include "BookParser.h"
+
+int main()
+{
+	std::string book = " Plenty of Room at the Bottom";
+	BookParserProxy* bookProxy = new BookParserProxy(book);
+
+	std::cout << bookProxy->requestNumberOfWords();
+
+	delete bookProxy;
+
+	return 0;
+}
